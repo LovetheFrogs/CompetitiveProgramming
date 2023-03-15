47a6eed5ab803f344c4ed49ee40a5dba881dfdcf
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-03-15 11:58:48 +0100

    Added UVa 573

---
+++ Competitive_Programming_3/Chapter_1/Section_133/Normal/UVa_00573/Main.java
@@ -0,0 +1,35 @@
+package Competitive_Programming_3.Chapter_1.Section_133.Normal.UVa_00573;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+
+        while ((s = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(s);
+            int h = Integer.parseInt(st.nextToken());
+            if (h == 0) break;
+            double u = Integer.parseInt(st.nextToken());
+            double d = Integer.parseInt(st.nextToken());
+            double f = Integer.parseInt(st.nextToken());
+            double curr = 0;
+            int day = 0;
+            double ff = u * f/100;
+            while (true) {
+                day++;
+                if (u > 0) curr += u;
+                u = u - ff;
+                if (curr > h) break;
+                curr = curr - d;
+                if (curr < 0) break;
+            }
+            if (curr < 0) System.out.println(new StringBuilder().append("failure on day ").append(day).toString());
+            else System.out.println(new StringBuilder().append("success on day ").append(day).toString());
+        }
+    }
+}
