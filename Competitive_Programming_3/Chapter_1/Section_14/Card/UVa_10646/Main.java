5214c482c135c6f0a9c7cc5d65ee25cb6dd27f39
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-03-29 10:51:21 +0200

    Added UVa 10646

---
+++ Competitive_Programming_3/Chapter_1/Section_14/Card/UVa_10646/Main.java
@@ -0,0 +1,17 @@
+package Competitive_Programming_3.Chapter_1.Section_14.Card.UVa_10646;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            String[] s = new String[52];
+            for (int j = 0; j < 52; j++) {
+                s[j] = sc.next();
+            }
+            System.out.println(new StringBuilder().append("Case ").append(i).append(": ").append(s[32]));
+        }
+    }
+}
