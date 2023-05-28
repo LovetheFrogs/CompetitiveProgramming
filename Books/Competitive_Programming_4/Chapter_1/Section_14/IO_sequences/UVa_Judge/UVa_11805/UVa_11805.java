654a38197858cbaa01bde85ba0ceee8b90657dd6
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-05-28 18:10:34 +0200

    Added problem from Uva id=11805

---
+++ Books/Competitive_Programming_4/Chapter_1/Section_14/IO_sequences/UVa_Judge/UVa_11805/UVa_11805.java
@@ -0,0 +1,25 @@
+package Books.Competitive_Programming_4.Chapter_1.Section_14.IO_sequences.UVa_Judge.UVa_11805;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class UVa_11805 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int t = Integer.parseInt(s);
+
+        for (int i = 0; i < t; i++) {
+            s = br.readLine();
+            StringTokenizer st = new StringTokenizer(s);
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            int sol = (p + k) % n;
+            if (sol == 0) sol = n;
+            System.out.println("Case " + (i + 1) + ": " + sol);
+        }
+    }
+}
