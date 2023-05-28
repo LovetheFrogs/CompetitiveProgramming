426d2ef02e9d0a402ed3b6b2891e7392706b1c15
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-05-28 09:47:48 +0200

    Added problem UVa_13025

---
+++ Books/Competitive_Programming_4/Chapter_1/Section_14/IO_sequences/UVa_Judge/UVa_13025/UVa_13025.java
@@ -0,0 +1,7 @@
+package Books.Competitive_Programming_4.Chapter_1.Section_14.IO_sequences.UVa_Judge.UVa_13025;
+
+public class UVa_13025 {
+    public static void main(String[] args) {
+        System.out.println("May 29, 2013 Wednesday");
+    }
+}
