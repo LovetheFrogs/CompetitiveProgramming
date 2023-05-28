18f177e70c61560ba7b64580b8e4cea24f817c93
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-05-28 10:02:55 +0200

    Fix: Typo in class name

--- Books/Competitive_Programming_4/Chapter_1/Section_14/IO_sequences/Kattis/Kattis_hello/hello.java
+++
@@ -1,7 +0,0 @@
-package Books.Competitive_Programming_4.Chapter_1.Section_14.IO_sequences.Kattis.Kattis_hello;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
