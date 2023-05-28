0581934edd31e78042489a4f722e08ca1bb8b0c2
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-05-28 19:21:56 +0200

    Added problem romans from Kattis

---
+++ Books/Competitive_Programming_4/Chapter_1/Section_14/IO_sequences/Kattis/Kattis_romans/Romans.java
@@ -0,0 +1,11 @@
+package Books.Competitive_Programming_4.Chapter_1.Section_14.IO_sequences.Kattis.Kattis_romans;
+
+import java.util.Scanner;
+
+public class Romans {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        System.out.println((int) Math.round(x * (1000 * (5280.0/4854.0))));
+    }
+}
