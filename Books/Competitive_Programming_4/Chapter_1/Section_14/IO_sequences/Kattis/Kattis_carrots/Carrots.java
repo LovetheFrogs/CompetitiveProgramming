b0d1fb2c7cf6a53036b9d3695a2d29f24b23b20a
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-05-28 10:02:40 +0200

    Added problem Kattis_Carrots

---
+++ Books/Competitive_Programming_4/Chapter_1/Section_14/IO_sequences/Kattis/Kattis_carrots/Carrots.java
@@ -0,0 +1,12 @@
+package Books.Competitive_Programming_4.Chapter_1.Section_14.IO_sequences.Kattis.Kattis_carrots;
+
+import java.util.Scanner;
+
+public class Carrots {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+        System.out.println(p);
+    }
+}
