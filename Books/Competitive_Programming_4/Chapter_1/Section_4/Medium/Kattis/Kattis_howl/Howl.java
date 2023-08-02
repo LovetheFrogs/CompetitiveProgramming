27734fba07d95f505ac32db21bbbc2cd76539b4b
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-08-02 15:01:20 +0200

    Added problem howl from Kattis

---
+++ Books/Competitive_Programming_4/Chapter_1/Section_4/Medium/Kattis/Kattis_howl/Howl.java
@@ -0,0 +1,11 @@
+package Books.Competitive_Programming_4.Chapter_1.Section_4.Medium.Kattis.Kattis_howl;
+
+import java.util.Scanner;
+
+public class Howl {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s + "O");
+    }
+}
